c6a937b796ab81f8f4168269a3c8c6935694925d
yiersansi5 <1179570889@qq.com>
2022-11-28 15:34:09 +0800

    完善

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,
+  css:{
+    loaderOptions:{
+      scss:{
+        additionalData:'@import "./src/styles/style.scss";'
+      }
+    }
+  },
+  configureWebpack:{
+    plugins:[
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }
 })
